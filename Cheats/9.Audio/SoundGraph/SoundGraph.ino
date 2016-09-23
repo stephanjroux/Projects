ba9124eb072d80f1545fe568531a54877daa6f20
stephanjroux <stephanjroux@gmail.com>
2016-09-23 06:11:56 +0200

    Session3

--- Cheats/9.Audio/SoundGraph/SoundGraph.ino
+++ Cheats/9.Audio/SoundGraph/SoundGraph.ino
@@ -1,39 +1,35 @@
 #include <Wire.h>
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
-#include <Average.h>
 
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
  
 const int maxScale = 8;
 const int redZone = 6;
  
-const int sampleWindow = 100; 
 const int cycleDelay = 100;
 
-Average<float> ave(sampleWindow);
+unsigned int signalMax = 900;
+unsigned int signalMin = 100;
+
 
 void setup() 
 {
    Serial.begin(115200);
    matrix.begin(0x70);  // pass in the address
+   Serial.println("Test Serial");
 }
  
  
 void loop() 
 {
-   unsigned int peakToPeak = 0;  
-   unsigned int signalMax = 560;
-   unsigned int signalMin = 460;
+   unsigned int sampleTot = 0;  
   
-   for(int x = 0; x < sampleWindow-1;x++) {
-      ave.push(analogRead(6));
-     }
-
-   Serial.print("Max: ");Serial.print(signalMax);Serial.print("\tMin: ");Serial.print(signalMin);Serial.print("\tAverage: ");Serial.print((int)ave.mean());
+   int getSample = analogRead(A0);
+   Serial.print("Max: ");Serial.print(signalMax);Serial.print("\tMin: ");Serial.print(signalMin);Serial.print("\tAverage: ");Serial.print(getSample);
    Serial.println();
 
-   int displayPeak = map(ave.mean(), signalMin, signalMax, 0, maxScale);
+   int displayPeak = map(getSample, signalMin, signalMax, 0, maxScale);
  
    // Update the display:
    for (int i = 0; i < 7; i++)  // shift the display left
