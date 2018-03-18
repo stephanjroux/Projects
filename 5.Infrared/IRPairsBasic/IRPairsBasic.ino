d158f34c97125b4c8ea1e3d639bdd6f7a55a4a41
stephanjroux <stephanjroux@gmail.com>
2018-03-18 10:59:32 +0200

    JPG Scehmatids
    Changed all Fritzing diagrams to JPGs

--- 5.Infrared/IRPairsBasic/IRPairsBasic.ino
+++ 5.Infrared/IRPairsBasic/IRPairsBasic.ino
@@ -1,11 +1,11 @@
- int photoPin = 3;
-
+int inputPin = 3;
+ 
  void setup() { 
-   pinMode (photoPin,INPUT_PULLUP);
+   pinMode (inputPin,INPUT_PULLUP);
    Serial.begin (115200);
  } 
 
  void loop() { 
-   Serial.println(digitalRead(photoPin));
-   delay(100);
- } 
+      Serial.println(digitalRead(inputPin));
+      delay(100);
+      } 
